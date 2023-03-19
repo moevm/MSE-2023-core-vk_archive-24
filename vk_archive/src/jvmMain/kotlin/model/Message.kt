27028c2b4ae0ac22fa541e7db310a6eebc7bc774
Dmitry Sergeev <pyukofe2013@gmail.com>
2023-03-19 23:20:12 +0300

    update models

--- vk_archive/src/jvmMain/kotlin/model/Message.kt
+++ vk_archive/src/jvmMain/kotlin/model/Message.kt
@@ -1,12 +1,10 @@
 package model
 
-import java.util.Date
-
 data class Message(
-        val authorId:Int,
-        val authorName:String,
-        val messageTime:Date,
-        val editTime:Date? = null,
-        val message:String? = null,
-        val attachments:List<Attachment> = listOf()
+        var authorId:String = "",
+        var authorName:String = "",
+        var messageTime:String = "",
+        var editTime:String? = null,
+        var message:String? = null,
+        var attachments:List<Attachment> = arrayListOf()
 )
